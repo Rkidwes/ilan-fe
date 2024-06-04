b8515a040921a9bb62a5b895610778d06441520c
Wes Moore <rkidwes@hotmail.com>
2024-06-04 12:16:58 +0100

    FEAT: Sync up Sanity with Home and Bio pages.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,31 @@
 import styles from "./page.module.scss";
 import Link from "next/link"
 import clsx from 'clsx'
-
 import Slider from './ui/slider/slider'
+import {PortableText} from '@portabletext/react'
 
-export default function Home() {
+import { sanityFetch } from "./sanity/client";
+
+const HOME_QUERY = `*[_type == "siteSettings"]{ hpTitle, hpText, hpLinkText, hpLinkURL }`;
+
+// Not sure if the below should be inside the function as it is on others - is it a significant difference?
+
+export default async function Home() {
+
+  const content = await sanityFetch({query: HOME_QUERY});
+  
+  const { hpTitle, hpText, hpLinkText, hpLinkURL } = content[0]
+
   return (
     <>
       <Slider />
       <main id={styles.main} className={clsx(`fill-height`, styles.home)} style={{backgroundImage: "url('https://www.ilanbluestone.com/assets/Bio/ilan-bio-section.jpg')"}}>
         <div className="container">
           <div className={clsx(`${styles.content}`, `${styles.contentNarrow}`)}>
-            <h2>Ilan&rsquo;s Story</h2>
-            <p>Since bursting onto Anjunabeats with his debut solo release back in 2012, London-based producer ilan Bluestone’s whirlwind journey through the international dance music scene has been nothing short of monumental.</p>
-            <p><Link href="/bio">Read the full story</Link></p>
+            <h2>{hpTitle}</h2>
+            <PortableText
+            value={hpText}></PortableText>
+            <p><Link href={hpLinkURL}>{hpLinkText}</Link></p>
           </div>
         </div>
       </main>
