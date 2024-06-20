f67333f59c346e80bff8f441461c005d2760cfd7
Wes Moore <rkidwes@hotmail.com>
2024-06-20 14:35:31 +0100

    FEAT: Another YT alternaive - this may actually work. Needs Play/Pause and Mute/Unmute buttons.

--- app/layout.js
+++ app/layout.js
@@ -92,6 +92,15 @@ export default function RootLayout({ children }) {
         {children}
         <Feature />
         <Footer />
+
+        <div className="yt-embed-holder-wrapper">
+          <div className="yt-embed-holder-inner-wrapper">
+          <div className="yt-embed-holder">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/-6DcDGbp2_k?si=ZPl6mNHx7Y3Toj5K&autoplay=1&mute=1&loop=1&controls=0&colo=white&modestbranding=0&rel=0&playsinline=1&enablejsapi=1&playlist=-6DcDGbp2_k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          </div>
+        </div>
+
       </body>
     </html>
   );
