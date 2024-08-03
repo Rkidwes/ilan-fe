341040b3976ad0cc9fb7af4394c6464dc0d93fd8
Wes Moore <rkidwes@hotmail.com>
2024-08-03 14:42:45 +0100

    Moving Events query, updating events page to test

---
+++ app/sanity/query.ts
@@ -0,0 +1,35 @@
+import { groq } from "next-sanity";
+
+export const postQuery = groq`*[_type == "post"] {
+  _id,
+  _createdAt,
+  title,
+  "slug": slug.current,
+  cover {
+    "image": asset->url,
+    "lqip": asset->metadata.lqip,
+    alt,
+  },
+  content,
+}`;
+
+export const singlePostQuery = groq`*[_type == "post" && slug.current == $slug][0] {
+  title,
+  content,
+}`;
+
+export const imagesQuery = groq`*[_type == "gallery"] {
+  _id,
+  image,
+  alt,
+  caption
+}|order(date desc)`;
+
+export const eventsQuery = groq`*[_type == "event"]{
+  _id, 
+  eventName, 
+  location, 
+  startDate, 
+  endDate, 
+  ticketsURL
+}|order(date desc)`;
