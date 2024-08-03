9d29802bfd2131b87d141ba62d662b798e612244
Wes Moore <rkidwes@hotmail.com>
2024-08-03 14:30:13 +0100

    Adds revalidation route

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,21 +1,29 @@
-import { revalidateTag } from "next/cache"; 
-import { NextResponse } from "next/server"; 
-import { parseBody } from "next-sanity/webhook"; 
+import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
+import { parseBody } from "next-sanity/webhook";
 
-export async function POST(req) { 
-  try { 
-    const { body, isValidSignature } = await parseBody(req, process.env.SANITY_REVALIDATE_SECRET); 
-    if (!isValidSignature) { 
-      return new Response("Invalid Signature", { status: 401 }); 
-    } 
-    if (!body?._type) { 
-      return new Response("Bad Request", { status: 400 }); 
-    } 
+export async function POST(req) {
+  try {
+    const { body, isValidSignature } = await parseBody(req, process.env.SANITY_REVALIDATE_SECRET);
+
+    if (!isValidSignature) {
+      return new Response("Invalid Signature", { status: 401 });
+    }
+
+    if (!body?._type) {
+      return new Response("Bad Request", { status: 400 });
+    }
+
     console.log('TYPE: ', body._type)
-    revalidateTag(body._type); 
-    return NextResponse.json({ status: 200, revalidated: true, now: Date.now(), body, }); 
-  } catch (error) { 
-    console.error(error); 
-    return new Response(error.message, { status: 500 }); 
-  } 
+    revalidateTag(body._type);
+    return NextResponse.json({
+      status: 200,
+      revalidated: true,
+      now: Date.now(),
+      body,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message, { status: 500 });
+  }
 }
